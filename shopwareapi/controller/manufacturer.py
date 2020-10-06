489991c49d1bfd649d55de3a05d156384bb6a693
msimmerlein <m.simmerlein@sachs-media>
2020-10-06 09:50:06 +0200

    implement get_or_create method

--- shopwareapi/controller/manufacturer.py
+++ shopwareapi/controller/manufacturer.py
@@ -4,3 +4,8 @@ from shopwareapi.core.basecontroller import BaseController
 class ManufacturerController(BaseController):
 
     api_model = "product-manufacturer"
+
+    def get_or_create(self, **kwargs):
+        if len(self.find(self.model.name, matches_field="name").all()) > 0:
+            return self.get(**kwargs)
+        return self.create(**kwargs)
