177ae734cc9e23fd5dc2e83a0fce1ad14a8d021c
msimmerlein <m.simmerlein@sachs-media>
2020-10-13 08:46:32 +0200

    Bump version: 0.0.29 → 0.0.30

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shopwareapi",
-    version="0.0.29",
+    version="0.0.30",
     author="Stefan Eiermann, Sachs Media",
     author_email="support@sachs-media.com",
     description="Provides a Python (object-based) API for Shopware",
