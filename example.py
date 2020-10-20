6cb0ba78c2460a7d02821d6e65b457d9ca13c61c
msimmerlein <m.simmerlein@sachs-media>
2020-10-20 09:36:57 +0200

    adding media attribute to product model

--- example.py
+++ example.py
@@ -11,63 +11,25 @@ from shopwareapi.models.media_configuration import MediaConfiguration
 from shopwareapi.models.media import Media
 import logging
 import uuid
+import requests
+import io
+import json
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 
 s = ShopwareClient(
-  base_url="http://janao.de",
-  version="v3",
-  client_id="SWIADTVMAHVTBTY1WJZYD3V2TA",
-  client_secret="eWU3MUQyZjJZQXd3ZHNMRjdSYVFhdWRQZ05IUUJxQUlDZ2VEWkk"
+    base_url="http://janao.de",
+    version="v3",
+    client_id="SWIADTVMAHVTBTY1WJZYD3V2TA",
+    client_secret="eWU3MUQyZjJZQXd3ZHNMRjdSYVFhdWRQZ05IUUJxQUlDZ2VEWkk"
 )
 
-# Media(
-#   **{
-#     "options": {"client": s}
-#   }
-# ).controller.create(options={"identifierName": "name"})
+endpoint = "http://janao.de/api/v3/"
+payload = {}
 
+headers = {
+    "Content-type": "application/json",
+    "Authorization": "Bearer {token}".format(token=s._get_token())
+}
 
-# cur = s.controller.Currency.find("EUR").all()[0]
-# tax = s.controller.Tax.find(19.0, matches_field="taxRate").all()[0]
-# sf = s.controller.SalesChannel.find("Storefront", matches_field="name").all()[0]
-# hs = s.controller.SalesChannel.find("Headless", matches_field="name").all()[0]
-
-# product_folder = s.controller.MediaFolder.find("Product Media", matches_field="name").all()[0]
-# media_configuration = s.controller.MediaConfiguration.find(True, matches_field="createThumbnails").all()[0]
-#
-# mediafolder = MediaFolder(**{
-#   "name": "földer_öne",
-#   "configurationId": media_configuration.id,
-#   "parentId": product_folder.id,
-#   "options": {"client": s}
-# }).controller.create(options={"identifierName": "name"})
-
-#
-# price = Price(**{
-#   "currency": cur,
-#   "gross": 150000,
-#   "net": 10,
-#   "linked": False,
-#   "options": {"client": s}
-# })
-#
-# Manufacturer(
-#   name="Aircraft",
-#   options={"client": s}
-# ).controller.get_or_create(options={"identifierName": "name"})
-#
-# test_manufacturer = s.controller.Manufacturer.find("Aircraft", matches_field="name").all()[0]
-# saleschannels = Queryset(SalesChannel, *[sf, hs])
-#
-# create_product = Product(
-#   name="asdfsdfadfdfgsgfhsghfgfgffgd",
-#   productNumber="89fe4b60-a9e5-455e-b2cc-8d8f6cacbca3",
-#   manufacturer=test_manufacturer,
-#   categories="",
-#   stock=10,
-#   tax=tax,
-#   price=[price],
-#   options={"client": s},
-#   visibilities=saleschannels
-# ).controller.get_or_create(options={"identifierName": "productNumber"})
--- shopwareapi/models/product.py
+++ shopwareapi/models/product.py
@@ -34,6 +34,7 @@ class Product(BaseModel):
         BaseField("taxId", "taxId", converter=Tax.convert, related_to="tax"),
         BaseField("manufacturer", "manufacturer", required=False, nested=True, converter=Manufacturer.convert),
         BaseField("manufacturerId", "manufacturerId", converter=Manufacturer.convert, related_to="manufacturer"),
+        BaseField("media", "media", required=False, nested=True),
         BaseField("productNumber", "productNumber", required=False),
         BaseField("stock", "stock", required=False, converter=Convert.to_int),
         BaseField("active", "active", required=False, converter=Convert.to_boolean),
@@ -55,5 +56,4 @@ class Product(BaseModel):
         BaseField("customFields", "customFields", required=False),
         BaseField("crossSelling", "crossSelling", required=False),
         BaseField("unit", "unit", required=False),
-        BaseField("media", "media", required=False),
     )
