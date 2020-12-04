2e82df4d78db94317451c1b9c1ec9ed8a52663bc
Stefan Eiermann <git@ultraapp.de>
2020-12-04 08:39:35 +0100

    add endpoints for tax and unit

--- shopwareapi/api/tax/tax_model.py
+++ shopwareapi/api/tax/tax_model.py
@@ -6,3 +6,8 @@ from shopwareapi.core.model import Model
 
 class TaxModel(Model):
     id = fields.UUIDField(primary_key=True, default=uuid.uuid4)
+    taxRate = fields.NumberField()
+    name = fields.CharField()
+
+    class Meta:
+        api_endpoint = "tax"
--- shopwareapi/api/unit/unit_model.py
+++ shopwareapi/api/unit/unit_model.py
@@ -6,3 +6,6 @@ from shopwareapi.core.model import Model
 
 class UnitModel(Model):
     id = fields.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    class Meta:
+        api_endpoint = "unit"
