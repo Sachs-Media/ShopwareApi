c3bfef00271cacd31b2b493a3b3bf4321e86d301
Stefan Eiermann <git@ultraapp.de>
2020-12-17 13:46:34 +0100

    hotfix

--- shopwareapi/api/category/category_model.py
+++ shopwareapi/api/category/category_model.py
@@ -6,7 +6,7 @@ from shopwareapi.core.model import Model
 
 class CategoryModel(Model):
     id = fields.UUIDField(primary_key=True, default=uuid.uuid4)
-    parent = fields.CharField()#fields.ForeignKey("self", null=True)
+    parent = fields.ForeignKey("self", null=True, related_name="parentId")
     name = fields.CharField(max_length=255)
 
     class Meta:
